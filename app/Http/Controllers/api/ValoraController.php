a4356ad5d0b9b1dc83cec669faabd444e23e5acb
Your Name <you@example.com>
2026-07-29 09:24:54 -0500

    detalles factura y valora

---
+++ app/Http/Controllers/api/ValoraController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Valora;
+
+class ValoraController extends Controller
+{
+    //
+    public function index()
+    {
+        $valoras = Valora::with('cita', 'cita.getBarbero:idUsuario,Nombre','cita.getCliente:idUsuario,Nombre')->get();
+        return response()->json($valoras);
+    }
+
+    public function show(string $id)
+    {
+        //
+        $valora = Valora::find($id);
+        if ($valora) {
+            return response()->json(['message' => 'Valoración no encontrada'], 404);
+        }
+
+        return response()->json($valora);
+    }
+
+    public function store(Request $request)
+    {
+        //
+        $valora = Valora::create($request->all());
+        return response()->json(['message' => 'Valoración creada correctamente', 'valora' => $valora], 201);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+        $valora = Valora::find($id);
+        if ($valora) {
+            $valora->update($request->all());
+            return response()->json(['message' => 'Valoración actualizada correctamente', 'valora' => $valora]);
+        } else {
+            return response()->json(['message' => 'Valoración no encontrada'], 404);
+        }
+    }
+
+    public function destroy(string $id)
+    {
+        //
+        $valora = Valora::find($id);
+        if ($valora) {
+            $valora->delete();
+            return response()->json(['message' => 'Valoración eliminada']);
+        } else {
+            return response()->json(['message' => 'Valoración no encontrada'], 404);
+        }
+    }
+}
