18eaad96a33693ade57d753d5bcd14ae9f4d9519
Your Name <you@example.com>
2026-07-29 09:36:18 -0500

    arreglo de rutas api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\CitasController;
 use App\Http\Controllers\api\ServicioController;
@@ -8,12 +7,14 @@ use App\Http\Controllers\api\usuarioController;
 use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\perfilController;
 use App\Http\Controllers\api\FacturaController;
+use App\Http\Controllers\api\ValoraController;
+use App\Http\Controllers\api\horarioController;
 
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-})->middleware('auth:sanctum');
+
 
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -25,11 +26,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('facturas', FacturaController::class);
     Route::apiResource('perfil', perfilController::class);
     Route::apiResource('valora', ValoraController::class);
+    Route::apiResource('horarios', horarioController::class);
 });
-
-Route::put('servicios/{id}', [ServicioController::class, 'update']);
-Route::delete('servicios/{id}', [ServicioController::class, 'destroy']);
-Route::apiResource('horarios', horarioController::class);
-
 
 
