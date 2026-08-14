45ccdd3edfbda86c76f36b958d60754b231fdd3a
MiWiMiN0 <qwejesusandres@gmail.com>
2026-08-13 20:27:03 -0500

    arrego de migracion de factura con su respectivo seeder

--- database/seeders/facturaSeeder.php
+++ database/seeders/facturaSeeder.php
@@ -17,24 +17,26 @@ class facturaSeeder extends Seeder
             [
                 'idfactura' => '1',
                 'numero_factura' => 'FAC-001',
-                'fecha_emision' => '2026-07-20',
+                'fecha_emision' => '2026-07-20 10:30:00',
                 'subtotal' => 50000,
                 'total_pagar' => 50000,
                 'metodo_pago' => 'Efectivo',
                 'estado_factura' => 'Emitida',
                 'Cita_idCita' => 1,
                 'Usuario_idUsuario' => 4,
+                "Servicio_idServicio" => 1,
             ],
             [
                 'idfactura' => '2',
                 'numero_factura' => 'FAC-002',
-                'fecha_emision' => '2026-07-21',
+                'fecha_emision' => '2026-07-21 15:45:00',
                 'subtotal' => 20000,
                 'total_pagar' => 20000,
                 'metodo_pago' => 'Nequi',
                 'estado_factura' => 'Emitida',
                 'Cita_idCita' => 2,
                 'Usuario_idUsuario' => 5,
+                "Servicio_idServicio" => 2,
             ],
         ]);
     }
